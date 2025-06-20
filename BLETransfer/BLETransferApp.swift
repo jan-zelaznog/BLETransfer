5c9013b49ec3a4f0d4152af2373cd816b9eb90ba
jan-zelaznog <jan.zelaznog@gmail.com>
2025-06-20 14:34:14 -0600

    Initial Commit

---
+++ BLETransfer/BLETransferApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLETransferApp.swift
+//  BLETransfer
+//
+//  Created by Ángel González on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct BLETransferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
